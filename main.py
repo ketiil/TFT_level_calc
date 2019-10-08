3f4ee593c072161d2436345866e12fcc7a3a0a1b
Ketil <ketilel@student.matnat.uio.no>
2019-10-08 15:58:36 +0200

    Refactoring 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import tkinter as tk
-from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E, N, Checkbutton, Frame, SUNKEN, LabelFrame
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E, N, Checkbutton, Radiobutton, Frame, SUNKEN, LabelFrame
 
 
 def hide_items(name):
@@ -46,25 +46,25 @@ class Gui:
 
         # Text fields #
         self.cur_lvl_label = Label(self.text_box, text="Current lvl:")
-        self.remaining_xp_lbl = Label(self.text_box, text="Remaining xp:")
-        self.cur_xp_lbl = Label(self.text_box, text="Current xp:")
-        self.cost_lbl = Label(self.text_box, text="Cost to level:")
+        self.remaining_xp_label = Label(self.text_box, text="Remaining xp:")
+        self.cur_xp_label = Label(self.text_box, text="Current xp:")
+        self.cost_label = Label(self.text_box, text="Cost to level:")
 
         self.cur_lvl_value_text = IntVar()
         self.cur_lvl_value_text.set(self.cur_lvl)
         self.cur_lvl_value = Label(self.text_box, textvariable=self.cur_lvl_value_text)
 
-        self.remaining_xp_label_text = IntVar()
-        self.remaining_xp_label_text.set(self.remaining_xp)
-        self.remaining_xp_label = Label(self.text_box, textvariable=self.remaining_xp_label_text)
+        self.remaining_xp_value_text = IntVar()
+        self.remaining_xp_value_text.set(self.remaining_xp)
+        self.remaining_xp_value = Label(self.text_box, textvariable=self.remaining_xp_value_text)
 
-        self.cur_xp_label_text = IntVar()
-        self.cur_xp_label_text.set(self.entered_xp)
-        self.cur_xp_label = Label(self.text_box, textvariable=self.cur_xp_label_text)
+        self.cur_xp_value_text = IntVar()
+        self.cur_xp_value_text.set(self.entered_xp)
+        self.cur_xp_value = Label(self.text_box, textvariable=self.cur_xp_value_text)
 
-        self.cost_label_text = IntVar()
-        self.cost_label_text.set(self.cost)
-        self.cost_label = Label(self.text_box, textvariable=self.cost_label_text)
+        self.cost_value_text = IntVar()
+        self.cost_value_text.set(self.cost)
+        self.cost_value = Label(self.text_box, textvariable=self.cost_value_text)
 
         vcmd = master.register(self.validate)  # we have to wrap the command
         self.cur_xp = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
@@ -95,42 +95,48 @@ class Gui:
         # XP Button
         self.xp_btn = Button(master, text="Enter xp", command=lambda: self.reg_xp(self.entered_xp))
 
+        # Radiobuttons (winstreaks)
+
+        
+
+
         # Checkbuttons
         self.win_streak = Checkbutton(master, text="Win streak")
         self.pirates = Checkbutton(master, text="Pirates")
 
-        ### LAYOUT ###
-
+        # LAYOUT #
 
         # Row 0
         self.cur_lvl_label.grid(row=0, column=1, sticky=W, pady=(5, 0))
         self.cur_lvl_value.grid(row=0, column=2, columnspan=2, sticky=E)
         hide_items(self.cur_lvl_label)
-        hide_items(self.cur_xp_label)
-        self.cur_lvl_label.grid_remove()
-        self.cur_lvl_value.grid_remove()
+        hide_items(self.cur_xp_value)
+        hide_items(self.cur_lvl_label)
+        hide_items(self.cur_lvl_value)
         # Row 1
-        self.remaining_xp_lbl.grid(row=1, column=1, sticky=W)
-        self.remaining_xp_label.grid(row=1, column=2, columnspan=2, sticky=E)
-        self.remaining_xp_lbl.grid_remove()
-        self.remaining_xp_label.grid_remove()
+        self.remaining_xp_label.grid(row=1, column=1, sticky=W)
+        self.remaining_xp_value.grid(row=1, column=4, columnspan=2, sticky=E)
+        hide_items(self.remaining_xp_label)
+        hide_items(self.remaining_xp_value)
         # Row 2
-        self.cur_xp_lbl.grid(row=2, column=1, sticky=W)
-        self.cur_xp_label.grid(row=2, column=2, columnspan=2, sticky=E)
-        self.cur_xp_lbl.grid_remove()
-        self.cur_xp_label.grid_remove()
+        self.cur_xp_label.grid(row=2, column=1, sticky=W)
+        self.cur_xp_value.grid(row=2, column=4, columnspan=2, sticky=E)
+        hide_items(self.cur_xp_label)
+        hide_items(self.cur_xp_value)
         # Row 3
-        self.cost_lbl.grid(row=3, column=1, sticky=W)
-        self.cost_label.grid(row=3, column=2, columnspan=2, sticky=E, pady=(0, 5))
-        self.cost_lbl.grid_remove()
-        self.cost_label.grid_remove()
+        self.cost_label.grid(row=3, column=1, sticky=W)
+        self.cost_value.grid(row=3, column=4, columnspan=2, sticky=E)
+        hide_items(self.cost_label)
+        hide_items(self.cost_value)
         # Row 5
         self.cur_xp.grid(row=5, column=1, columnspan=2, sticky=W + E, padx=5)
         self.xp_btn.grid(row=5, column=3)
 
+        # Checkbox alternatives
         self.win_streak.grid(row=6, column=1, sticky=W)
         self.pirates.grid(row=7, column=1, sticky=W)
 
+        # Close
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.grid(row=9, column=3)
 
@@ -153,14 +159,14 @@ class Gui:
         show_items(self.cur_lvl_value)
 
         self.remaining_xp = self.level_xp[self.cur_lvl-1]
-        self.remaining_xp_label_text.set(self.remaining_xp)
+        self.remaining_xp_value_text.set(self.remaining_xp)
 
-        show_items(self.remaining_xp_lbl)
         show_items(self.remaining_xp_label)
+        show_items(self.remaining_xp_value)
 
     def reg_xp(self, xp):
         self.entered_xp = int(xp)
-        self.cur_xp_label_text.set(self.entered_xp)
+        self.cur_xp_value_text.set(self.entered_xp)
         self.calc_diff()
         self.cur_xp.delete(0, END)  # reset textbox
 
@@ -168,25 +174,25 @@ class Gui:
         xp_needed = self.level_xp[self.cur_lvl-1]
         self.remaining_xp = (xp_needed - self.entered_xp)
         if self.remaining_xp >= 0:
-            self.remaining_xp_label_text.set(self.remaining_xp)
-        else:
-            self.remaining_xp_label_text.set(0)
+            self.remaining_xp_value_text.set(self.remaining_xp)
+        elif self.remaining_xp < 0:
+            self.remaining_xp_value_text.set(0)
         self.calc_cost()
 
     def calc_cost(self):
         if self.remaining_xp%4 == 0:
             self.cost = self.remaining_xp/4
-            self.cost_label_text.set(self.cost*4)
+            self.cost_value_text.set(self.cost * 4)
         elif self.remaining_xp%4 <= 4:
             remainder = self.remaining_xp % 4
             if remainder < 4:
                 self.cost = 1 + self.remaining_xp//4
-                self.cost_label_text.set(self.cost*4)
+                self.cost_value_text.set(self.cost * 4)
 
-        show_items(self.cur_xp_lbl)
         show_items(self.cur_xp_label)
-        show_items(self.cost_lbl)
+        show_items(self.cur_xp_value)
         show_items(self.cost_label)
+        show_items(self.cost_value)
 
 
 
